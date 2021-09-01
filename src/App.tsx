0f5c0d6179b32c3a90f8dd02b83b509dd8724a71
Dale B <daleban.dev@gmail.com>
2021-09-01 21:42:10 +0800

    Call Public API using axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const [data, setData] = useState<any>();
+  const getApi = async () => {
+    try {
+      const url = "https://api.publicapis.org/random";
+      const res = await axios.get(url);
+      setData(res.data.entries[0]);
+      console.log(res.data.entries[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
-      <span></span>
+      <span onClick={() => getApi()}>test lol {data.API}</span>
     </div>
   );
 }
