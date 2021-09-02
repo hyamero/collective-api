a226d11c9a8972aff8753af448941cd9227a3124
Dale B <daleban.dev@gmail.com>
2021-09-02 15:01:24 +0800

    Display all entries data

---
+++ src/components/ApiCard.tsx
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+import React from "react";
+import { ApiDetails } from "./ApiDetails";
+
+interface ApiCardProps {
+  data: any;
+  showContent: boolean;
+}
+
+export const ApiCard: React.FC<ApiCardProps> = ({ data, showContent }) => {
+  return (
+    <div
+      className="ApiCard container"
+      css={css`
+        height: 100vh;
+        width: 100vw;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+        gap: 2rem;
+        margin: auto;
+
+        .api-card {
+          background: coral;
+        }
+      `}
+    >
+      {/* All entries */}
+      {showContent &&
+        data.map((api: any) => {
+          return (
+            <div className="api-card" key={api.Link}>
+              <ApiDetails api={api} />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
---
+++ src/components/ApiDetails.tsx
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+import React from "react";
+
+interface ApiCardProps {
+  api: any;
+}
+
+export const ApiDetails: React.FC<ApiCardProps> = ({ api }) => {
+  return (
+    <ul css={css``}>
+      <li>{api.API}</li>
+      <li>{api.Category}</li>
+      <li>{api.Description}</li>
+      <li>link</li>
+      <li>{api.Auth === "" ? "no auth" : api.Auth}</li>
+      <li>{api.HTTPS}</li>
+      <li>{api.Cors}</li>
+    </ul>
+  );
+};
