34031a94dd909df230fc05785521482ef75ab235
Dale B <daleban.dev@gmail.com>
2021-09-02 15:02:12 +0800

    Import all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
+/** @jsx jsx */
+import { css, jsx, Global } from "@emotion/react";
 import React, { useState, useEffect } from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
+import "css-maid";
+
+//components
+import { Navbar } from "./components/Navbar";
+import { LandingPage } from "./components/LandingPage";
+import { CategoryResult } from "./components/CategoryResult";
+import { AllEntries } from "./components/AllEntries";
+import { SearchResult } from "./components/SearchResult";
+import { ApiCard } from "./components/ApiCard";
 
 function App() {
   useEffect(() => {
-    getApi();
+    getAllEntries();
+    setTimeout(() => {
+      setShowContent(true);
+    }, 3000);
   }, []);
 
   const [data, setData] = useState<any>();
-  const getApi = async () => {
+  const [showContent, setShowContent] = useState<boolean>(false);
+  const getAllEntries = async () => {
     try {
-      const url = "https://api.publicapis.org/random";
+      const url = "https://api.publicapis.org/entries";
       const res = await axios.get(url);
-      setData(res.data.entries[0]);
-      console.log(res.data.entries[0]);
+      setData(res.data.entries);
+      console.log(res.data.entries);
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <div className="App">
-      <span onClick={() => getApi()}>test lol {data.API}</span>
+    <div
+      className="App"
+      css={css`
+        height: 100vh;
+      `}
+    >
+      <Navbar />
+      <ApiCard data={data} showContent={showContent} />
+      <Global
+        styles={css`
+          * {
+            margin: auto;
+            padding: none;
+            box-sizing: border-box;
+          }
+
+          .api-card {
+            /* display: inline-block; */
+            width: 350px;
+          }
+
+          .container {
+            width: 80%;
+            max-width: 1280px;
+            margin: auto;
+          }
+        `}
+      />
     </div>
   );
 }
