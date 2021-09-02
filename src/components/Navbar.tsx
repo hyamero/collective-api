71f04fc3775d54eb242314cc16e123af11b6d89a
Dale B <daleban.dev@gmail.com>
2021-09-02 18:34:26 +0800

    Add Navbar component on the DOM.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,36 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import React from "react";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
   return (
-    <nav>
-      <span></span>
+    <nav
+      className="Navbar container"
+      css={css`
+        display: flex;
+        height: 100px;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 25px;
+
+        h1 {
+          font-size: 2rem;
+        }
+
+        .link-github {
+          font-size: 1.3rem;
+        }
+      `}
+    >
+      <h1>Collective APIs</h1>
+      <a
+        className="link-github"
+        href="https://github.com/hyamero/collective-api"
+      >
+        github
+      </a>
     </nav>
   );
 };
