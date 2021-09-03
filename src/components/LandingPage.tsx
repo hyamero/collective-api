fbcb324f6e48a792cd157b367a41be38adf21e15
Dale B <daleban.dev@gmail.com>
2021-09-03 15:58:09 +0800

    Minor organization on category list.

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,11 +9,31 @@ interface LandingPageProps {
 
 export const LandingPage: React.FC<LandingPageProps> = ({ categories }) => {
   return (
-    <header className="LandingPage container">
+    <header
+      className="LandingPage container"
+      css={css`
+        .categories-container {
+          line-height: 1.3rem;
+          margin-bottom: 10px;
+        }
+
+        .category {
+          &:not(:last-child) {
+            margin-right: 10px;
+          }
+
+          &:not(:last-child)::after {
+            content: "  |";
+          }
+        }
+      `}
+    >
       <h1>get started by choosing a category</h1>
-      {categories.map((category: any) => (
-        <span>{category}</span>
-      ))}
+      <div className="categories-container">
+        {categories.map((category: any) => (
+          <span className="category">{category}</span>
+        ))}
+      </div>
       <div className="header-btns">
         <Link to="/category/:name">
           <h3>show all APIs instead</h3>
