bf3354413dcc85549b4e329a3afd4ef20a2237a9
Dale B <daleban.dev@gmail.com>
2021-09-09 10:38:07 +0800

    Initialize navbar design.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,30 +15,53 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
         justify-content: space-between;
         align-items: center;
         margin-bottom: 25px;
+        padding-top: 25px;
+
+        .btn-nav {
+          padding: 0 60px;
+          font-weight: 600;
+          font-size: 0.8rem;
+          color: #303030;
+        }
 
         .logo {
-          font-size: 2rem;
+          font-size: 1.2rem;
+          font-weight: 700;
+          color: #000;
           margin: 0;
         }
 
         .link-github {
-          font-size: 1.3rem;
+          font-size: 0.8rem;
+          font-weight: 700;
           margin: 0;
+          background: #fff;
+          border-radius: 15px;
+          padding: 10px 30px;
         }
       `}
     >
       <Link to="/" className="logo">
         <h1>Collective APIs</h1>
       </Link>
-      <div className="link-github">
-        <a
-          className="link-github"
-          href="https://github.com/hyamero/collective-api"
-          target="_blank"
-        >
-          github
-        </a>
+      <div className="btns-navbar">
+        <Link className="btn-nav" to="/">
+          Home
+        </Link>
+        <Link className="btn-nav" to="/information">
+          Information
+        </Link>
+        <Link className="btn-nav" to="/all-results">
+          Find APIs
+        </Link>
       </div>
+      <a
+        className="link-github"
+        href="https://github.com/hyamero/collective-api"
+        target="_blank"
+      >
+        GitHub Repo
+      </a>
     </nav>
   );
 };
