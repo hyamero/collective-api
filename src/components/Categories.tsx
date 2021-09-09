86f627dd1ec7ee8b00b00a8b7c6a8681bea63faa
Dale B <daleban.dev@gmail.com>
2021-09-09 17:42:04 +0800

    Add categories design.

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,6 +2,8 @@
 import { css, jsx } from "@emotion/react";
 import React from "react";
 
+import { FiSearch } from "react-icons/fi";
+
 interface CategoriesProps {
   categories: any;
 }
@@ -9,19 +11,37 @@ interface CategoriesProps {
 export const Categories: React.FC<CategoriesProps> = ({ categories }) => {
   return (
     <div
-      className="Categories container"
+      className="Categories"
       css={css`
-        display: flex;
-        flex-flow: row wrap;
+        width: 80%;
+        max-width: 1280px;
 
-        p {
-          margin: 20px;
+        .categories-container p {
+          text-align: center;
+          font-size: 0.8rem;
+          font-weight: 400;
+          color: #fff;
+          margin: 10px;
+          width: 210px;
+          background: linear-gradient(#b33cff, #c168fa);
+          box-shadow: 1px 1px 2px #c168fa;
+          padding: 10px 0;
+          border-radius: 10px;
         }
       `}
     >
-      {categories.map((category: string | undefined) => (
-        <p>{category}</p>
-      ))}
+      <div
+        className="categories-container"
+        css={css`
+          display: flex;
+          flex-flow: row wrap;
+          justify-content: center;
+          background: #e0e0e0;
+        `}
+      >
+        {categories !== undefined &&
+          categories.map((category: string | undefined) => <p>{category}</p>)}
+      </div>
     </div>
   );
 };
