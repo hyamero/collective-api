dd6fb3309fbfc75ee1dd1cd674ad45669c46cdae
Dale B <daleban.dev@gmail.com>
2021-09-10 18:01:22 +0800

    Organize category result entries data.

--- src/components/CategoryEntry.tsx
+++ src/components/CategoryEntry.tsx
@@ -9,12 +9,48 @@ interface CategoryEntryProps {
 
 export const CategoryEntry: React.FC<CategoryEntryProps> = ({ entry }) => {
   return (
-    <div>
-      <div className="api-title">
-        <h3>{entry.API}</h3>
-        <FiExternalLink />
+    <div
+      className="CategoryEntry"
+      css={css`
+        .api-title-category {
+          display: flex;
+          justify-content: space-between;
+
+          .api-title {
+            display: flex;
+          }
+        }
+
+        ul {
+          display: flex;
+          justify-content: flex-start;
+        }
+
+        .entry-description {
+          margin: 5px 0;
+          font-size: 0.9rem;
+        }
+      `}
+    >
+      <div className="api-title-category">
+        <div className="api-title">
+          <h3>{entry.API}</h3>
+          <FiExternalLink />
+        </div>
+        <span>{entry.Category}</span>
       </div>
-      <p>{entry.Description}</p>
+      <p className="entry-description">{entry.Description}</p>
+      <ul>
+        <li>
+          Auth: <span>{entry.Auth === "" ? "No Auth" : entry.Auth}</span>
+        </li>
+        <li>
+          HTTPS: <span>{entry.HTTPS.toString()}</span>
+        </li>
+        <li>
+          Cors: <span>{entry.Cors}</span>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/components/CategoryResult.tsx
+++ src/components/CategoryResult.tsx
@@ -16,14 +16,26 @@ export const CategoryResult: React.FC<CategoryResultProps> = ({
       css={css`
         height: 100vh;
         width: 100vw;
-        position: absolute;
         top: 0;
+        padding: 50px;
+        position: absolute;
         background-image: linear-gradient(to top, #cd9cf2 0%, #f6f3ff 100%);
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        overflow-x: hidden;
+        padding-top: 150px;
+
+        .category-data-container {
+          height: 155px;
+          width: 440px;
+          margin-bottom: 50px;
+        }
       `}
     >
       {categoryData !== null && categoryData !== undefined ? (
         categoryData.map((entry: any) => (
-          <div key={entry.Link}>
+          <div key={entry.Link} className="category-data-container">
             <CategoryEntry entry={entry} />
           </div>
         ))
